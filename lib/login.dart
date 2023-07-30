f6e75797380cf7c697bd8050174ff8dbac839548
ireshanananda <73053441+ireshanananda@users.noreply.github.com>
2023-07-30 20:57:12 +0530

    login updated

--- lib/login.dart
+++ lib/login.dart
@@ -20,6 +20,9 @@ TextEditingController _emailTextController = TextEditingController();
 var _isObscured = true;
 
 class _LoginState extends State<Login> {
+  String? _emailError;
+  String? _passwordError;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -75,17 +78,19 @@ class _LoginState extends State<Login> {
                                   },
                                   style: TextStyle(color: Colors.black),
                                   decoration: InputDecoration(
-                                      suffixIcon: Padding(
-                                        padding: EdgeInsetsDirectional.only(
-                                            end: 10.0),
-                                        child: Icon(Icons.mail),
-                                      ),
-                                      hintText: "Email",
-                                      fillColor: Colors.grey.shade100,
-                                      filled: true,
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      )),
+                                    suffixIcon: Padding(
+                                      padding:
+                                          EdgeInsetsDirectional.only(end: 10.0),
+                                      child: Icon(Icons.mail),
+                                    ),
+                                    hintText: "Email",
+                                    fillColor: Colors.grey.shade100,
+                                    filled: true,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    errorText: _emailError,
+                                  ),
                                 ),
                                 SizedBox(
                                   height: 30,
@@ -104,24 +109,26 @@ class _LoginState extends State<Login> {
                                   style: TextStyle(),
                                   obscureText: _isObscured,
                                   decoration: InputDecoration(
-                                      suffixIcon: IconButton(
-                                        padding: EdgeInsetsDirectional.only(
-                                            end: 10.0),
-                                        icon: _isObscured
-                                            ? Icon(Icons.visibility)
-                                            : Icon(Icons.visibility_off),
-                                        onPressed: () {
-                                          setState(() {
-                                            _isObscured = !_isObscured;
-                                          });
-                                        },
-                                      ),
-                                      fillColor: Colors.grey.shade100,
-                                      filled: true,
-                                      hintText: "Password",
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      )),
+                                    suffixIcon: IconButton(
+                                      padding:
+                                          EdgeInsetsDirectional.only(end: 10.0),
+                                      icon: _isObscured
+                                          ? Icon(Icons.visibility)
+                                          : Icon(Icons.visibility_off),
+                                      onPressed: () {
+                                        setState(() {
+                                          _isObscured = !_isObscured;
+                                        });
+                                      },
+                                    ),
+                                    fillColor: Colors.grey.shade100,
+                                    filled: true,
+                                    hintText: "Password",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    errorText: _passwordError,
+                                  ),
                                 ),
                                 SizedBox(
                                   height: 10,
@@ -155,24 +162,73 @@ class _LoginState extends State<Login> {
                                           width: 150,
                                           child: ElevatedButton(
                                             onPressed: () async {
+                                              String email =
+                                                  _emailTextController.text
+                                                      .trim();
+                                              String password =
+                                                  _passwordTextController.text;
+
+                                              if (email.isEmpty) {
+                                                setState(() {
+                                                  _emailError =
+                                                      "Please Enter Your Email";
+                                                  _passwordError = null;
+                                                });
+                                                return;
+                                              }
+
+                                              if (!RegExp(
+                                                      r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-z]")
+                                                  .hasMatch(email)) {
+                                                setState(() {
+                                                  _emailError =
+                                                      "Please Enter a valid Email";
+                                                  _passwordError = null;
+                                                });
+                                                return;
+                                              }
+
+                                              if (password.isEmpty) {
+                                                setState(() {
+                                                  _passwordError =
+                                                      "Password is required for login";
+                                                  _emailError = null;
+                                                });
+                                                return;
+                                              }
+
+                                              RegExp regex = RegExp(r'^.{6,}$');
+                                              if (!regex.hasMatch(password)) {
+                                                setState(() {
+                                                  _passwordError =
+                                                      "Please enter valid password (min. 6 characters)";
+                                                  _emailError = null;
+                                                });
+                                                return;
+                                              }
+
+                                              // If the validation passes, attempt to sign in.
                                               await FirebaseAuth.instance
                                                   .signInWithEmailAndPassword(
-                                                      email:
-                                                          _emailTextController
-                                                              .text
-                                                              .trim(),
-                                                      password:
-                                                          _passwordTextController
-                                                              .text)
+                                                      email: email,
+                                                      password: password)
                                                   .then((value) {
+                                                // Success, navigate to the next screen.
                                                 Navigator.push(
                                                     context,
                                                     MaterialPageRoute(
                                                         builder: (context) =>
                                                             TwoFactorAuthScreen()));
                                               }).onError((error, stackTrace) {
+                                                // Error during sign-in, print the error.
                                                 print(
                                                     "Error ${error.toString()}");
+                                                // Show a generic error message.
+                                                setState(() {
+                                                  _passwordError =
+                                                      "Invalid email or password";
+                                                  _emailError = null;
+                                                });
                                               });
                                             },
                                             child: Text(
